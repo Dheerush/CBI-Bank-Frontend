a943ea9dc036b450bb9905c785e4d330db37f304
Dheeraj Sharma <sharmadheeraj1996@gmail.com>
2025-07-08 23:28:58 +0530

    Use env variable for backend base URL

--- src/utils/AxiosClient.jsx
+++ src/utils/AxiosClient.jsx
@@ -4,10 +4,20 @@
 //   baseURL:process.env.NEXT_PUBLIC_BASE_URL,
 // });
 
+// import axios from "axios";
+
+// export const axiosClient = axios.create({
+//   baseURL: "http://localhost:1234/api/v1",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:1234/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL, // ✅ Dynamic base URL
   headers: {
     "Content-Type": "application/json",
   },
